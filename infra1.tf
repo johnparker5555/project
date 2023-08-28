4bbfa85de8ee6da2d7d96c8700387d440f7199a1
john <127201342+johnparker5555@users.noreply.github.com>
2023-08-28 18:34:21 +0530

    Update infra1.tf

--- infra1.tf
+++ infra1.tf
@@ -20,11 +20,3 @@ resource "aws_instance" "myec2" {
     Name = "HelloWorld"
   }
 }
-
-resource "aws_vpc" "my-vpc" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
-  tags = {
-    Name = "myvpc"
-  }
-}
